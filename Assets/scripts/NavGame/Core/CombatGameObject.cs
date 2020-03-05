8b7b94f998c0ddce093cf080934a3c4ea806530b
Thiago Henrique <thiagohenriquelemoscosta@gmail.com>
2020-03-05 10:18:34 -0300

    Basic combat system

---
+++ Assets/scripts/NavGame/Core/CombatGameObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NavGame.Core {
+    public class CombatGameObject : DamageableGameObject {
+
+        float cooldown = 0f;
+    
+        protected virtual void Update()
+        {
+            DecreaseAttackCooldown();
+        }
+        public void AttackOnCooldown(DamageableGameObject target)
+        {
+            if (cooldown <= 0f)
+            {
+                cooldown = 1/stats.attackSpeed;
+                target.TakeDamage(stats.damage);
+            }
+        }
+
+        void DecreaseAttackCooldown()
+        {
+            if (cooldown == 0f)
+            {
+                return;
+            }
+            cooldown = cooldown - Time.deltaTime;
+            if (cooldown < 0f)
+            {
+                cooldown = 0f;
+            }
+        }
+    }
+}
--- Assets/scripts/characters/CreepController.cs
+++ Assets/scripts/characters/CreepController.cs
@@ -2,23 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using NavGame.Core;
 
 [RequireComponent(typeof(NavMeshAgent))]
-public class CreepController : MonoBehaviour
+public class CreepController : CombatGameObject
 {
     NavMeshAgent agent;
-    GameObject finalTarget;
-    void Awake()
+    DamageableGameObject finalTarget;
+    protected override void Awake()
     {
+        base.Awake();
         agent = GetComponent<NavMeshAgent>();
         GameObject obj = GameObject.FindWithTag("Finish");
         if(obj != null)
         {
-            finalTarget = obj;
+            finalTarget = obj.GetComponent<DamageableGameObject>();
         }
     }
 
-   
+   protected override void Update() 
+   {
+       base.Update();
+       if(finalTarget == null)
+       {
+           return;
+       }
+       if (IsInTouch(finalTarget))
+       {
+           AttackOnCooldown(finalTarget);
+       }
+   }
     void Start()
     {
         if(finalTarget != null)
