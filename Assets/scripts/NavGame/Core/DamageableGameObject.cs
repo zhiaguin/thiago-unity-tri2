265739769211f4a734de5d569f96612b49e5bed7
Thiago Henrique <thiagohenriquelemoscosta@gmail.com>
2020-03-17 11:03:48 -0300

    HealthUI prefab, health changed and died events

--- Assets/scripts/NavGame/Core/DamageableGameObject.cs
+++ Assets/scripts/NavGame/Core/DamageableGameObject.cs
@@ -6,6 +6,8 @@ namespace NavGame.Core {
     public class DamageableGameObject : TouchableGameObject {
         public int currentHealth;
         public Stats stats;
+        public OnHealthChangedEvent onHealthChanged;
+        public OnDiedEvent onDied;
 
         protected virtual void Awake()
         {
@@ -18,6 +20,10 @@ namespace NavGame.Core {
             amount = Mathf.Clamp(amount, 1, stats.maxHealth);
 
             currentHealth = currentHealth - amount;
+            if (onHealthChanged != null)
+            {
+                onHealthChanged(stats.maxHealth, currentHealth);
+            }
 
             if(currentHealth <=0 )
             {
@@ -28,6 +34,10 @@ namespace NavGame.Core {
         public virtual void Die()
         {
             Destroy(gameObject);
+            if(onDied != null)
+            {
+                onDied();
+            }
         }
     }
 }
