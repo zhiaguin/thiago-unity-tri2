6a96faa1b04c7eef5b105269cfb44320f0f1e171
Thiago Henrique <thiagohenriquelemoscosta@gmail.com>
2020-04-01 23:51:33 -0300

    Moving NavMeshAgent to AttackGameObject

--- Assets/scripts/characters/CreepController.cs
+++ Assets/scripts/characters/CreepController.cs
@@ -5,14 +5,14 @@ using UnityEngine.AI;
 using NavGame.Core;
 using NavGame.Managers;
 
-[RequireComponent(typeof(NavMeshAgent))]
+
 public class CreepController : AttackGameObject
 {
-    NavMeshAgent agent;
+    
     DamageableGameObject finalTarget;
-    void Awake()
+    protected override void Awake()
     {
-        agent = GetComponent<NavMeshAgent>();
+        base.Awake();        
         GameObject obj = GameObject.FindWithTag("Finish");
         if(obj != null)
         {
@@ -25,14 +25,14 @@ public class CreepController : AttackGameObject
    protected override void Update() 
    {
        base.Update();
-       if(finalTarget == null)
+       if(finalTarget != null)
        {
-           return;
-       }
-       if (IsInTouch(finalTarget))
-       {
-           AttackOnCooldown(finalTarget);
-       }
+            if (IsInTouch(finalTarget))
+            {
+                AttackOnCooldown(finalTarget);
+            }
+        }
+       
    }
     void Start()
     {
