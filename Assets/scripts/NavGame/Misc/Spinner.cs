35b28eb09d4326f0a58f2b6b483454283a492a77
Thiago Henrique <thiagohenriquelemoscosta@gmail.com>
2020-04-30 08:45:10 -0300

    Projectile Spinner

---
+++ Assets/scripts/NavGame/Misc/Spinner.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace NavGame.Misc
+{
+    public class Spinner : MonoBehaviour
+    {
+        public Vector3 eulersPerSecond;
+        void Update()
+        {
+            transform.Rotate(eulersPerSecond * Time.deltaTime);
+        }
+    }
+}
