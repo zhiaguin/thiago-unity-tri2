0911604c8307d0a9e80499f299cd203f984f5b8c
Thiago Henrique <thiagohenriquelemoscosta@gmail.com>
2020-04-30 08:29:54 -0300

    Attack events

--- Assets/scripts/NavGame/Core/AttackGameObject.cs
+++ Assets/scripts/NavGame/Core/AttackGameObject.cs
@@ -12,6 +12,8 @@ namespace NavGame.Core
 
         public OfenseStats ofenseStats;
         public float attackRange = 4f;
+        public float attackDelay = 0.5f;
+        public Transform castTransform;
         public string[] enemyLayers;
 
         [SerializeField]
@@ -23,12 +25,21 @@ namespace NavGame.Core
         float cooldown = 0f;
         LayerMask enemyMask;
 
-        public OnAttackHitEvent onAttackHit;
+        public OnAttackStartEvent onAttackStart;
+
+        public OnAttackCastEvent onAttackCast;
+
+        public OnAttackStrikeEvent onAttackStrike;
 
         protected virtual void Awake()
         {
             agent = GetComponent<NavMeshAgent>();
             enemyMask = LayerMask.GetMask(enemyLayers);
+            if(castTransform == null)
+            {
+                castTransform = transform;
+            }
+
         }
 
         protected virtual void Update()
@@ -56,13 +67,32 @@ namespace NavGame.Core
             if (cooldown <= 0f)
             {
                 cooldown = 1f / ofenseStats.attackSpeed;
-                target.TakeDamage(ofenseStats.damage);
-                if (onAttackHit != null)
+                if (onAttackStart != null)
                 {
-                    onAttackHit(target.transform.position);
+                    onAttackStart();
+                }
+                StartCoroutine(AttackAfterDelay(target, attackDelay));
+               
+            }
+        }
+        IEnumerator AttackAfterDelay(DamageableGameObject target, float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            if(target!=null)
+            {
+                if (onAttackCast != null)
+                {
+                    onAttackCast(castTransform.position);
                 }
 
-            }
+                target.TakeDamage(ofenseStats.damage);
+
+                if(onAttackStrike != null)
+                {
+                    onAttackStrike(target.damageTransform.position);
+                }
+               
+            }          
         }
 
         void DecreaseAttackCooldown()
