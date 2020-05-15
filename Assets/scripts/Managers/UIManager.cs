06efed69e4fe03ac8eb992ed61d244056af691f1
Thiago Henrique <thiagohenriquelemoscosta@gmail.com>
2020-05-15 01:39:20 -0300

    Wave countdown event

--- Assets/scripts/Managers/UIManager.cs
+++ Assets/scripts/Managers/UIManager.cs
@@ -11,6 +11,7 @@ public class UIManager : MonoBehaviour
     public float errorTime = 1.5f;
     public Text coinText;
     public Text waveCountText;
+    public Text waveCountdownText;
     public GameObject[] cooldownObjects;
     public Text[] actionCosts;
 
@@ -24,6 +25,7 @@ public class UIManager : MonoBehaviour
         LevelManager.instance.onResourceUpdate += OnResourceUpdate;
         LevelManager.instance.onReportableError += OnReportableError;
         LevelManager.instance.onWaveUpdate += OnWaveUpdate;
+        LevelManager.instance.onWaveCountdown += OnWaveCountdown;
     }
 
     void Start()
@@ -76,6 +78,11 @@ public class UIManager : MonoBehaviour
     void OnWaveUpdate(int totalWaves, int currentWave)
     {
         waveCountText.text = currentWave + " / " + totalWaves;
+    }
+
+    void OnWaveCountdown (float remainingTime)
+    {
+        waveCountdownText.text = remainingTime.ToString("F1");
     }
 
     IEnumerator TurnOffError()
